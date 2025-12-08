7f92132297da37ce0ba7399767a84ed6b20b23c8
Dhruvi Kapadia <kapadia.dhr@northeastern.edu>
2025-12-08 15:54:32 -0500

    Update 05_Data_Analyst_Home.py

--- app/src/pages/05_Data_Analyst_Home.py
+++ app/src/pages/05_Data_Analyst_Home.py
@@ -27,7 +27,7 @@ if st.button('View User Data & Analysis',
 if st.button('View Movie Data & Analysis', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/01_App_Vers_History.py')
+  st.switch_page('pages/08_Movie_Analytics.py')
 
 if st.button('Check your messages with the team', 
              type='primary',
