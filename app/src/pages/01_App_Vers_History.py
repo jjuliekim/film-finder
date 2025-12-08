63c56d972cf688ae7fa109193cbc7597a6645a06
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2025-12-08 18:22:59 -0500

    fix app version history

--- app/src/pages/01_App_Vers_History.py
+++ app/src/pages/01_App_Vers_History.py
@@ -7,10 +7,12 @@ logger = logging.getLogger(__name__)
 
 SideBarLinks()
 
-st.markdown(f" ## <span style='color:blue;'> Film Finder Admin </span>", unsafe_allow_html=True)
+st.markdown(
+    f" ## <span style='color:blue;'> Film Finder Admin </span>", unsafe_allow_html=True
+)
 st.markdown(" #### App Versions")
 
-url = 'http://api:4000/employee/versions'
+url = "http://api:4000/employee/versions"
 
 try:
     response = requests.get(url)
@@ -20,23 +22,29 @@ try:
     if versions_data:
         current_version = versions_data[0]
         
-        st.write(f"**Current Active Version: {current_version.get('version_num', 'Unknown')}**")
-        st.write(f"Deployed on {current_version.get('publishedAt', 'Unknown')}")
-        
         st.markdown("---")
         st.markdown(" #### History")
 
-        for v in versions_data[1:]:
+        for v in versions_data[0:]:
             col1, col2 = st.columns([3, 1])
-            
+
             with col1:
-                version_info = f"**{v.get('version_num')}**:  \nDeployed on {v.get('publishedAt')}"
+                version_info = f"**Version {v.get('versionID')}**:  \nPublished on {v.get('publishedAt')}  \n_{v.get('description', 'No description')}_"
                 st.write(version_info)
-            
+
             with col2:
-                if st.button("Restore", key=f"restore_{v.get('id')}"):
-                    st.success(f"Restoring version {v.get('version_num')}...")
-            
+                version_id = v.get("versionID")
+                if st.button("Restore", key=f"restore_{version_id}"):
+                    try:
+                        restore_url = f"http://api:4000/employee/versions/{version_id}"
+                        restore_response = requests.put(restore_url)
+                        restore_response.raise_for_status()
+
+                        st.success(f"Successfully restored version {version_id}!")
+                        st.rerun()
+                    except Exception as restore_error:
+                        st.error(f"Error restoring version: {restore_error}")
+
             st.divider()
 
     else:
